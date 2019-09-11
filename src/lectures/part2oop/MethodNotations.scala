86ca41a3d09065d52fa974fdb94c720298a90450
robinsonvs <robinsonvs@gmail.com>
2019-09-11 08:55:28 -0300

    add MethodNotation exercises

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -2,13 +2,18 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(nickName: String): Person = new Person(s"$name ($nickName)", favoriteMovie)
     def unary_! : String = s"$name, what the heck?!"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+    def apply(n: Int): String = s"$name watched $favoriteMovie $n times"
+    def learns(thing: String) = s"$name is leaning $thing"
+    def learnsScala = this learns "Scala"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -43,5 +48,26 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary()) // equivalent
+
+  /*
+    1.  Overload the + operator
+        mary + "the rockstar" => new Person "Mary (the rockstar)"
+
+    2.  Add an age to the Person class
+        Add a unary + operator => person with the age + 1
+        +mary => mary with the age incrementer
+
+    3.  Add a "learns" method in the Person class => "Mary learns Scala"
+        Add a learnsScala method, calls learns method with "Scala".
+        Use it in postfix notation.
+
+    4.  Overload the apply method
+        mary.apply(2) => "Mary watched Inception 2 times"
+   */
+
+  println((mary + "the rockstar").apply()) // 1
+  println((+mary).age) // 2
+  println(mary learnsScala) // 3
+  println(mary(10)) // 4
 
 }
