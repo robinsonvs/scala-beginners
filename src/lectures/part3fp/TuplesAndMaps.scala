86b7c81b5838799a56ad36263d28b6f1cab30d89
robinsonvs <robinsonvs@gmail.com>
2020-11-14 02:20:26 -0300

    add tuples and maps exercises

--- src/lectures/part3fp/TuplesAndMaps.scala
+++ src/lectures/part3fp/TuplesAndMaps.scala
@@ -1,5 +1,9 @@
 package lectures.part3fp
 
+import java.nio.channels.NetworkChannel
+
+import scala.annotation.tailrec
+
 object TuplesAndMaps extends App {
 
   //tuples = finite ordered "lists"
@@ -12,7 +16,7 @@ object TuplesAndMaps extends App {
   //Maps keys > values
   val aMap: Map[String, Int] = Map()
 
-  val phonebook = Map(("Jim", 555), "Daniel" -> 789).withDefaultValue(-1)
+  val phonebook = Map(("Jim", 555), "Daniel" -> 789, ("JIM", 9000)).withDefaultValue(-1)
   //a -> b is sugar for (a,b)
   println(phonebook)
 
@@ -41,6 +45,101 @@ object TuplesAndMaps extends App {
   val names = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
   println(names.groupBy(name => name.charAt(0)))
 
-  
+  /*
+    1. What would happen if I had two original entries
+        "Jim" -> 555 and "JIM" -> 900 ?
+
+       !!! careful with mapping keys
+
+    2. Overly simplified social network based on maps
+        Person = String
+        - add a person to the network
+        - remove
+        - friend (mutual)
+        - unfriend
+
+        - number of friends of a person
+        - person with most friends
+        - how many people have NO friends
+        - if there is a social connection between two people (direct or not)
+
+   */
+
+  def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] =
+    network + (person -> Set())
+
+  def friend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA + b)) + (b -> (friendsB + a))
+  }
+
+  def unfriend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA - b)) + (b -> (friendsB - a))
+  }
+
+  def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] =
+      if (friends.isEmpty) networkAcc
+      else removeAux(friends.tail, unfriend(networkAcc, person, friends.head))
+
+    val unfriended = removeAux(network(person), network)
+    unfriended - person
+  }
+
+  val empty: Map[String, Set[String]] = Map()
+  val network = add(add(empty, "Bob"), "Mary")
+  println(network)
+  println(friend(network, "Bob", "Mary"))
+  println(unfriend(friend(network, "Bob", "Mary"), "Bob", "Mary"))
+  println(remove(friend(network, "Bob", "Mary"), "Bob"))
+
+  //Jim, Bob, Mary
+  val people = add(add(add(empty, "Bob"), "Mary"), "Jim")
+  val jimBob = friend(people, "Bob", "Jim")
+  val testNet = friend(jimBob, "Bob", "Mary")
+
+  println(testNet)
+
+  def numberFriends(network: Map[String, Set[String]], person: String): Int =
+    if (!network.contains(person)) 0
+    else (network(person).size)
+
+  println(numberFriends(testNet, "Bob"))
+
+  def mostFriends(network: Map[String, Set[String]]): String =
+    network.maxBy(pair => pair._2.size)._1
+
+  println(mostFriends(network))
+
+  def numberPeopleWithNoFriends(network: Map[String, Set[String]]): Int =
+    //network.filterKeys(k => network(k).isEmpty).size
+    network.count(_._2.isEmpty)
+
+  println(numberPeopleWithNoFriends(testNet))
+
+  def socialConnection(network: Map[String, Set[String]], a: String, b: String): Boolean = {
+    // breadth-first search
+    @tailrec
+    def bfs (target: String, consideredPeople: Set[String], discoveredPeople: Set[String]): Boolean = {
+      if (discoveredPeople.isEmpty) false
+      else {
+        val person = discoveredPeople.head
+        if (person == target) true
+        else if (consideredPeople.contains(person)) bfs(target, consideredPeople, discoveredPeople.tail)
+        else bfs(target, consideredPeople + person, discoveredPeople.tail ++ network(person))
+      }
+    }
+
+    bfs(b, Set(), network(a) + a)
+  }
+
+  println(socialConnection(testNet, "Mary", "Jim"))
+  println(socialConnection(network, "Mary", "Bob"))
+
 
 }
